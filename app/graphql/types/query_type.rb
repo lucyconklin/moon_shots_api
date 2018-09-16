ad30d33844c941d92410b80e2465ae94b7b029d3
Lucy Conklin <lucy.conklin@gmail.com>
2018-09-16 11:16:21 -0400

    Adds basic sorter

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,8 +2,11 @@ Types::QueryType = GraphQL::ObjectType.define do
   name "Query"
 
   field :barrels, !types[Types::BarrelType] do
+    argument :order, types.String
+    argument :attribute, types.String
+    
     resolve -> (obj, args, ctx) {
-      Barrel.all
+      BarrelSorter.new(args[:attribute], args[:order]).sort
     }
   end
   
@@ -11,5 +14,23 @@ Types::QueryType = GraphQL::ObjectType.define do
     resolve -> (obj, args, ctx) {
       Satellite.all
     }
+  end
+end
+
+class BarrelSorter
+  def initialize(attribute = "id", order = "asc")
+    @attribute = attribute.to_sym
+    @order = order.upcase
+  end
+  
+  def sort
+    barrels = Barrel.all
+    barrels = barrels.order(@attribute)
+    
+    if @order = "DESC"
+      barrels = barrels.reverse
+    end
+    
+    barrels
   end
 end
